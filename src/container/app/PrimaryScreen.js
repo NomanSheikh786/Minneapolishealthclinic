275bb49c9faddda1dce9b0fd68eca3864851ead9
Muhammad Mudassir <61446880+mudassir089@users.noreply.github.com>
2022-05-30 01:57:52 -0700

    updated code 30/5

--- src/container/app/PrimaryScreen.js
+++ src/container/app/PrimaryScreen.js
@@ -6,6 +6,7 @@ import {
   SafeAreaView,
   Image,
   TouchableOpacity,
+  ScrollView,
 } from 'react-native';
 import {vh} from '../../constaint';
 
@@ -39,68 +40,70 @@ function PrimaryScreen({route}) {
             borderColor: '#F9F9F9',
             width: 75,
             alignSelf: 'center',
-            marginVertical: 15,
+            marginVertical: vh / 50,
             borderRadius: 50,
           }}
         />
-        <View
-          style={{
-            paddingHorizontal: 20,
-            // flex: 1,
-            justifyContent: 'center',
-          }}>
-          <Text
+        <ScrollView contentContainerStyle={{paddingBottom: vh / 7}}>
+          <View
             style={{
-              color: '#FFF',
-              fontSize: 25,
-              fontWeight: '500',
-            }}>
-            {state?.name}
-          </Text>
-          <Text
-            numberOfLines={5}
-            style={{
-              color: '#FFF',
-              fontSize: 14,
-              opacity: 0.7,
-              marginTop: vh / 40,
-              lineHeight: 20,
-            }}>
-            {state?.des}
-          </Text>
-          <Text
-            numberOfLines={2}
-            style={{
-              fontSize: 16,
-              color: '#FFF',
-              marginTop: vh / 40,
-              paddingRight: 10,
-            }}>
-            {state.title}
-          </Text>
-          <TouchableOpacity
-            onPress={() => console.log('hello')}
-            activeOpacity={0.6}
-            style={{
-              backgroundColor: '#FFF',
-              borderRadius: 8,
-              height: vh / 14,
-              alignItems: 'center',
+              paddingHorizontal: 20,
+              // flex: 1,
               justifyContent: 'center',
-              marginTop: vh / 20,
             }}>
-            <Text style={{color: '#FE2F50', fontSize: 16, fontWeight: '500'}}>
-              Click Here
+            <Text
+              style={{
+                color: '#FFF',
+                fontSize: 22,
+                fontWeight: '500',
+              }}>
+              {state?.name}
             </Text>
-          </TouchableOpacity>
-        </View>
+            <Text
+              numberOfLines={5}
+              style={{
+                color: '#FFF',
+                fontSize: 12,
+                opacity: 0.7,
+                marginTop: vh / 40,
+                lineHeight: 15,
+              }}>
+              {state?.des}
+            </Text>
+            <Text
+              numberOfLines={2}
+              style={{
+                fontSize: 16,
+                color: '#FFF',
+                marginTop: vh / 40,
+                paddingRight: 10,
+              }}>
+              {state.title}
+            </Text>
+            <TouchableOpacity
+              onPress={() => console.log('hello')}
+              activeOpacity={0.6}
+              style={{
+                backgroundColor: '#FFF',
+                borderRadius: 8,
+                height: vh / 14,
+                alignItems: 'center',
+                justifyContent: 'center',
+                marginTop: vh / 20,
+              }}>
+              <Text style={{color: '#FE2F50', fontSize: 16, fontWeight: '500'}}>
+                Click Here
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </ScrollView>
       </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {flex: 1},
+  container: {flex: 1, backgroundColor: '#FFF'},
 });
 
 export default PrimaryScreen;
