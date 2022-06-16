d9c9ea2585d37551fc0bdd9cfc2cbc3fdb2685f7
Muhammad Mudassir <61446880+mudassir089@users.noreply.github.com>
2022-06-16 08:38:14 -0700

    letest code

--- App.js
+++ App.js
@@ -1,19 +1,43 @@
-import React from 'react';
-import {View, Text, ImageBackground, StyleSheet, StatusBar} from 'react-native';
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  ImageBackground,
+  StyleSheet,
+  StatusBar,
+  TouchableOpacity,
+} from 'react-native';
 import Navigation from './src/navigation/stack/index';
 import {NativeBaseProvider} from 'native-base';
-
+import AgoraUIKit, {mode, role} from 'agora-rn-uikit';
 import {LogBox} from 'react-native';
 const App = () => {
+  const [videoCall, setVideoCall] = useState(true);
+  const rtcProps = {
+    appId: '90133bcd4f1a4c459699ca2934973349',
+    channel: 'test',
+  };
+  const callbacks = {
+    EndCall: () => setVideoCall(false),
+  };
   LogBox.ignoreLogs([
     'Warning: Async Storage has been extracted from react-native core',
   ]);
 
-  return (
-    <NativeBaseProvider>
-      <StatusBar backgroundColor={'#FFF'} barStyle="dark-content" />
-      <Navigation />
-    </NativeBaseProvider>
+  return videoCall ? (
+    <AgoraUIKit
+      rtcProps={{
+        appId: '90133bcd4f1a4c459699ca2934973349',
+        channel: 'test',
+        mode: mode.LiveBroadcasting,
+        role: role.Broadcaster,
+      }}
+      callbacks={callbacks}
+    />
+  ) : (
+    <View>
+      <Text onPress={() => setVideoCall(true)}>Start Call</Text>
+    </View>
   );
 };
 
